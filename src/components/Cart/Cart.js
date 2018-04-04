05a5bd1c314ef1b79959309376c674cb1e0646ba
DriscollRichie <driscollrichie@gmail.com>
2018-04-04 15:24:34 -0600

    Nav bar cosmetic update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Nav from '../../components/Nav/Nav'
 
 export default class Cart extends Component {
    
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import './Nav.css'
+import logo from '../../hollister_Logo.png'
 
 export default function() {
    return (
       <div>
          <nav className='nav'>
-            <span>Hollister</span>
-            <ul className='HeadCat'>
+            <ul className='headCat'>
                <li>Guys</li>
-               <li>Girls</li>
+               <li className='headGirl'>Girls</li>
+            </ul>
+            <div className='title'>
+               <span className="title_text">Hollister</span>
+               <img src={logo} className='app_logo' alt="logo"/>           
+            </div>
+            <ul className='account_actions'>
+               <li>Account</li>
+               <li className='cart'>Cart</li>
             </ul>
          </nav>
       </div>
